aec6b16aa2f2d18eb1e675b982c3d11541311ff3
edafeE <edafe.edemo@cecula.com>
2019-05-23 15:19:01 +0100

    Isomophic App Project

---
+++ README.md
@@ -0,0 +1,23 @@
+#Isomorphics App
+Clone and Enjoy
+###Algorithm in action:
+If you like algorithm then this repo would interest you to clone. It is a simple program that takes in to strings as parameter and map one to another . It returns true if it is and return false if it is not.
+
+###The Problem in details:
+Given two (dictionary) words as Strings, determine if they are isomorphic. Two words are called isomorphic
+if the letters in one word can be remapped to get the second word. Remapping a letter means replacing all
+occurrences of it with another letter while the ordering of the letters remains unchanged. No two letters
+may map to the same letter, but a letter may map to itself.
+
+### The Logic:
+(1) Check length of both the strings
+
+(2) If strings are of not equal length then return false
+
+(3) Create a character map use characters from string 1 as key and map it with string 2
+
+(4) While iterating over the string if string 1’s character does not exist in the hash map then add it to the hash map and map it to the string 2’s value
+
+(5) If it exists then compare it with the string 2’s character
+
+### Happy Coding!!!
---
+++ index.js
@@ -0,0 +1,68 @@
+
+//input DOM element
+const input1 = document.querySelector('input:nth-of-type(1)');
+const input2 = document.querySelector('input:nth-of-type(2)');
+
+// error and success DOM element
+const errorEl = document.querySelector('.error');
+const resultEl = document.querySelector('.result')
+
+// input value string
+let elStr1 ;
+let elStr2 ;
+
+// Catch the values from input element on keyup even
+input1.addEventListener('keyup',()=>{
+    elStr1 = input1.value;
+    console.log(elStr1)
+});
+
+input2.addEventListener('keyup',()=>{
+    elStr2 = input2.value;
+    console.log(elStr2)
+});
+
+// The ismophicLogic Function recieves two parameters
+
+const isomophicLogic = (str1,str2)=>{
+
+    const str1Len = str1.length;
+
+      if(str1Len != str2.length) return resultEl.textContent = 'Error: strings not equal in length';
+    
+      const HashMapObj = {}
+    
+      for(let i=0; i < str1Len; i++){
+        if(!HashMapObj[str1[i]]){
+          HashMapObj[str1[i]] = str2[i]
+        }
+         else if( HashMapObj[str1[i]] !== str2[i]){
+    
+          console.log('Both strings differ in mapping at index ' + i);
+          
+           return resultEl.innerHTML = `Both strings differ in mapping so therefore:<br><span class="success">${false}</span>`;
+        }
+      }
+      
+      console.log(HashMapObj)
+      input1.value = "";
+      input2.value="";
+      return resultEl.innerHTML = `<span class="success pd">${true}</span>`
+    }
+
+//Fire up the Event listener when the button is clicked and pass in the argument to the isomorphic function
+
+    document.querySelector('button').addEventListener('click',()=>{
+    
+        if(elStr1 === undefined || elStr2 === undefined ){
+            return errorEl.textContent = "Please fill in the form"
+        }
+        else if(elStr1.length =="" || elStr1.length ==""){
+            resultEl.textContent="";
+            return errorEl.textContent = "Please Dont Clear the input before you submit"
+        }
+        else{
+            isomophicLogic(elStr1,elStr2);
+            errorEl.textContent = ""
+        }
+    })
